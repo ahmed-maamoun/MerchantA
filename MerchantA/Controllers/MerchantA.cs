5d2cb9907155d7b2dc47d444592e603f7cdaa649
Ahmed Maamoun <ahmed.maamoun@tpay.me>
2022-08-01 13:37:29 +0200

    Add project files.

---
+++ MerchantA/Controllers/MerchantA.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MerchantA.Controllers
+{
+    [Route("api/user/")]
+    [ApiController]
+    public class MerchantA : ControllerBase
+    {
+        [HttpPost("activate")]
+        public ActionResult Activate(ActivateDTO activateDTO) {
+            return Ok();
+        }
+        [HttpPost("terminate")]
+        public ActionResult Terminate(SuspendDTO suspendDTO)
+        {
+            return Ok();
+        }
+        [HttpPost("suspend")]
+        public ActionResult Suspend(SuspendDTO suspendDTO)
+        {
+            return Ok();
+        }
+        [HttpPost("reactivate")]
+        public ActionResult Reactivate(SuspendDTO suspendDTO)
+        {
+            return Ok();
+        }
+    }
+    public class ActivateDTO { 
+        public string PhoneNumber { get; set; }
+        public string Package { get; set; }
+        public string OperatorName { get; set; }
+    }
+
+    public class SuspendDTO
+    {
+        public string PhoneNumber { get; set; }
+        public string Package { get; set; }
+    }
+}
